cb44be88a67adfd5fee954addac2619eb9a4c0ce
github username <github email address>
2021-02-24 14:02:04 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,11 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+
+    def initialize(hash)
+        hash.each do |k,v|
+            self.class.attr_accessor(k.to_s)
+            self.send("#{k}=",v)
+        end
+    end
+
+end
